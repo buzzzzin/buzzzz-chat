0a4afd96a8056a35402acf88a0c4e4e98dac41fe
Jitendra Singh Bisht <jitendra.singh@tothenew.com>
2015-09-26 03:59:54 +0530

    Main Class added

---
+++ src/main/java/in/buzzzz/ChatApplication.java
@@ -0,0 +1,14 @@
+package in.buzzzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jitendra on 26/9/15.
+ */
+@SpringBootApplication
+public class ChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+}
