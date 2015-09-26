609bea73b5d68b509c47c5778be104cbf925498d
Jitendra Singh Bisht <jitendra.singh@tothenew.com>
2015-09-26 19:17:39 +0530

    Web Socket endpoints configured to application.yaml file

--- src/main/java/in/buzzzz/config/websocket/WebSocketConfig.java
+++ src/main/java/in/buzzzz/config/websocket/WebSocketConfig.java
@@ -4,12 +4,17 @@ import in.buzzzz.context.ChannelContextHolder;
 import in.buzzzz.context.WebSocketContextHolder;
 import in.buzzzz.messaging.BuzzWebSocketHandler;
 import in.buzzzz.messaging.PayloadHandler;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Assert;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import java.util.List;
 
 /**
  * @author jitendra on 25/9/15.
@@ -17,10 +22,19 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+    Logger logger = Logger.getLogger(getClass().getName());
+
+    @Value("${buzz.ws.endpoints}")
+    List<String> wsEndpoints;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        Assert.notEmpty(wsEndpoints);
+        String urls[] = new String[wsEndpoints.size()];
+        urls = wsEndpoints.toArray(urls);
+        logger.info(String.format("Registering Web Socket endpoints -- %s --", wsEndpoints));
         registry
-                .addHandler(webSocketHandler(), "/buzz/chat/**", "/buzz/publish/**", "/buzz/channel/**")
+                .addHandler(webSocketHandler(), urls)
                 .setAllowedOrigins("*");
     }
 
